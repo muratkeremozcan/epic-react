bff29f52d220095c48a588e639cdc54b4937427e
Murat Ozcan <muratkerem@gmail.com>
2022-06-26 12:09:36 +0300

    7.3 done

--- react-hooks/src/exercise/02.js
+++ react-hooks/src/exercise/02.js
@@ -4,17 +4,29 @@
 import * as React from 'react'
 
 function Greeting({initialName = ''}) {
+  // [2] TL,DR;
+  // (0) any time the component renders, useEffect is called
+  // (1) useState can take a fn as an argument which runs once on mount
+  // used for initial, costly calculations for initial state, so that they don't repeat on setState
+  // (2) useEffect takes a second argument, the "dependency array", which signals to React
+  // that the useEffect callback function should only called when those dependencies change
+  // (3) custom hooks are just reuseable functions that help us tidy up the code
+
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
-
+  // const [name, setName] = React.useState(
+  //   () => window.localStorage.getItem('name') ?? initialName,
+  // )
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
+  // React.useEffect(() => window.localStorage.setItem('name', name), [name])
 
-  function handleChange(event) {
-    setName(event.target.value)
-  }
+  // (3) the above can be made into a custom hook
+  const [name, setName] = useLocalStorageState('name', initialName)
+
+  const handleChange = event => setName(event.target.value)
+
   return (
     <div>
       <form>
@@ -24,6 +36,19 @@ function Greeting({initialName = ''}) {
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
   )
+}
+
+function useLocalStorageState(key, defaultValue = '') {
+  const [state, setState] = React.useState(
+    () => window.localStorage.getItem('name') ?? defaultValue,
+  )
+
+  React.useEffect(
+    () => window.localStorage.setItem(key, String(state)),
+    [key, state],
+  )
+
+  return [state, setState]
 }
 
 function App() {
