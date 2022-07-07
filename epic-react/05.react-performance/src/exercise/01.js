082483920ff146da291f1eaad10e40a3215df887
Murat Ozcan <muratkerem@gmail.com>
2022-07-07 07:06:20 +0300

    perf section 5

---
+++ epic-react/05.react-performance/src/exercise/01.js
@@ -0,0 +1,58 @@
+// Code splitting
+// http://localhost:3000/isolated/exercise/01.js
+
+import * as React from 'react'
+// 💣 remove this import
+// import Globe from '../globe'
+
+// [1] React provides an easy way to specify fallback UI: the Suspense component
+// Use the Suspense component to wrap UI that contains one or more lazy components in its tree
+
+// 🐨 use React.lazy to create a Globe component which uses a dynamic import
+// to get the Globe component from the '../globe' module.
+const loadGlobe = () => import(/* webpackPrefetch: true */ '../globe')
+const Globe = React.lazy(loadGlobe)
+
+function App() {
+  const [showGlobe, setShowGlobe] = React.useState(false)
+
+  // 🐨 wrap the code below in a <React.Suspense /> component
+  // with a fallback.
+  // 💰 try putting it in a few different places and observe how that
+  // impacts the user experience.
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        height: '100%',
+        padding: '2rem',
+      }}
+    >
+      <label
+        style={{marginBottom: '1rem'}}
+        onMouseEnter={loadGlobe}
+        onFocus={loadGlobe}
+      >
+        <input
+          type="checkbox"
+          checked={showGlobe}
+          onChange={e => setShowGlobe(e.target.checked)}
+        />
+        {' show globe'}
+      </label>
+      <div style={{width: 400, height: 400}}>
+        <React.Suspense fallback={<div>loading globe...</div>}>
+          {showGlobe ? <Globe /> : null}
+        </React.Suspense>
+      </div>
+    </div>
+  )
+}
+// 🦉 Note that if you're not on the isolated page, then you'll notice that this
+// app actually already has a React.Suspense component higher up in the tree
+// where this component is rendered, so you *could* just rely on that one.
+
+export default App
