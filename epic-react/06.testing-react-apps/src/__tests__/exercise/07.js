eb12429a78c71632625814744dde03edd9e182f3
Murat Ozcan <muratkerem@gmail.com>
2022-07-09 16:38:49 +0300

    07

--- epic-react/06.testing-react-apps/src/__tests__/exercise/07.js
+++ epic-react/06.testing-react-apps/src/__tests__/exercise/07.js
@@ -6,17 +6,47 @@ import {render, screen} from '@testing-library/react'
 import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
-test('renders with the light styles for the light theme', () => {
-  // 🐨 uncomment all of this code and your test will be busted on the next line:
-  // render(<EasyButton>Easy</EasyButton>)
-  // const button = screen.getByRole('button', {name: /easy/i})
-  // expect(button).toHaveStyle(`
-  //   background-color: white;
-  //   color: black;
-  // `)
-  //
-  // 🐨 update the `render` call above to use the wrapper option using the
-  // ThemeProvider
+test('renders with the dark styles for the light theme - raw', () => {
+  render(
+    <ThemeProvider initialTheme="light">
+      <EasyButton>Easy</EasyButton>,
+    </ThemeProvider>,
+  )
+
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+  background-color: white;
+  color: black;
+  `)
+})
+
+test('renders with the dark styles for the light theme', () => {
+  const Wrapper = ({children}) => (
+    <ThemeProvider initialTheme="light">{children}</ThemeProvider>
+  )
+  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+  background-color: white;
+  color: black;
+  `)
 })
 
 /* eslint no-unused-vars:0 */
+const renderWithProviders = (ui, {theme = 'light', ...options} = {}) => {
+  const Wrapper = ({children}) => (
+    <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
+  )
+  return render(ui, {wrapper: Wrapper, ...options})
+}
+
+test('renders with the light styles for the dark theme', () => {
+  renderWithProviders(<EasyButton>Easy</EasyButton>, {theme: 'dark'})
+  const button = screen.getByRole('button', {name: /easy/i})
+
+  expect(button).toHaveStyle(`
+  background-color: black;
+  color: white;
+  `)
+})
